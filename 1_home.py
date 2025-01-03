ade9b0aedcfc6534cefac6092adb615f46828780
DiegoDouglasRodrigues <diego.rodrigues2013dr@gmail.com>
2025-01-03 17:08:28 -0300

    projeto rodando finalizado ajuste de barras teste bd

--- 1_home.py
+++ 1_home.py
@@ -7,7 +7,9 @@ import pprint
 st.write(" # FIFA23 Dados Oficiais")
 
 if "data" not in st.session_state:
-    df_data = pd.read_csv(r"C:/Users/DIEGO/Documents/GitHub/Streamlit_1app/datasets/FIFA23_official_data.csv",index_col=0)
+    df_data = pd.read_csv("datasets/FIFA23_official_data.csv", index_col=0)
+
+    #df_data = pd.read_csv(r"C:/Users/DIEGO/Documents/GitHub/Streamlit_1app/datasets/FIFA23_official_data.csv",index_col=0)
     # df_data = df_data[df_data["Contract Valid Until"] >= datetime.today().year]
     # df_data = df_data[df_data["Value(€)"] >0]
     df_data = df_data.sort_values(by="Overall", ascending=False)
